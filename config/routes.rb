46066a7dad3faf05b45ac85dc6f7b45878b356bf
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2021-12-25 03:27:26 +0000

    [update] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about"
+
+  resources :users, only: [:show, :index, :edit, :update]
+  resources :books
+
+
 end
