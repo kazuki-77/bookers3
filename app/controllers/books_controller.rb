1f04b2368c43233b5691f8997201714c9eeab8ab
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2021-12-27 03:22:17 +0000

    [update] 4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,66 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def show
+    @book = Book.new
+    @books = Book.find(params[:id])
+    @user = @books.user #投稿したユーザーを@userに代入している
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id #この本は誰が投稿するのかを宣言しないといけない。関連付ける箇所は、本を作る時である。つまり、books_contorollerのcreateアクション内でに記載する必要がある。
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @books = Book.all #これを入れないとrenderしたときにエラーが起きる
+      @user = current_user#上と同じくrenderしたときに定義されていないというエラーが起きる
+      render :index
+    end
+
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.user = current_user #もし、投稿したユーザーとログインしているユーザーが同じなら編集画面に行く
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
+
+  private
+
+  def correct_user
+    user = Book.find(params[:id])
+    if current_user.id != user.id
+      redirect_to books_path
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,48 @@
 class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
-  end
-
-  def create
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user #もし、ユーザー詳細画面とログインしているユーザーが同じだったら、編集画面を開く
+      render :edit
+    else
+      redirect_to user_path(id: current_user)
+    end
+
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
+
   end
 
-  def destroy
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introducrion, :profile_image)
   end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user #編集画面のユーザーとログインしているユーザーが同じでなかったら
+      redirect_to user_path(current_user) #ログインしているユーザーの詳細ページ（マイページ）に遷移する
+    end
+  end
+
 end
